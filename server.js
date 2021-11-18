f13e202db65ebd0565fd1cf9d0bcfad66f2cc301
Hailey Thomas <haileyraebeauty@gmail.com>
2021-11-17 19:21:58 -0700

    start post request function

--- server.js
+++ server.js
@@ -69,6 +69,13 @@ app.get('/api/animals/:id', (req, res) => {
     }
 });
 
+// define a route that listens for POST requests
+app.post('/api/animals', (req, res) => {
+    // req.body is where our incoming content will be
+    console.log(req.body);
+    res.json(req.body);
+});
+
 // make server listen for connections on the specified host and port
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
